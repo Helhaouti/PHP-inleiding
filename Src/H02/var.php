b732958b31c9b638bb59decac2045aa49a65a9c4
Helhaouti <H.haouti@hotmail.com>
2020-03-25 20:58:32 +0100

    H02

--- Src/H02/var.php
+++ Src/H02/var.php
@@ -3,16 +3,22 @@ $a1 = "Ik heb niet gefaald";
 $b1 = "die niet werken";
 $c1 = "10000";
 
-echo $a1 . ". Ik heb alleen " . $c1 . " manieren " . $b1 . "<br>";
+echo $a1 . ". Ik heb alleen " . $c1 . " manieren " . $b1 . "<br>-Thomas Edison<br><br>";
 
 $a2 = "die geen fout";
 $b2 = "en";
 $c2 = "niet.";
 
-echo "Mensen " . $a2 . $b2 . " maken, werken " . $c2 . "<br>";
+echo "Mensen " . $a2 . $b2 . " maken, werken " . $c2 . "<br>-Albert Plesman (KLM)<br><br>";
 
 $a3 = "ry";
 $b3 = ", but";
 $c3 = "fail";
 
-echo "T" . $a3 . " and " . $c3 . $b3 . " never " . $c3 . " to t" . $a3 . ".";
+echo "T" . $a3 . " and " . $c3 . $b3 . " never " . $c3 . " to t" . $a3 . "." . "<br>-Tyra Banks<br><br>";
+
+$a4 = '"omgaan';
+$b4 = "met";
+$c4 = "teleu";
+
+echo "De cursus " . $a4 . " " . $b4 . " " . $c4 . 'rstellingen” kan vanavond helaas niet doorgaan.' . "<br>-Herman Finkers";
