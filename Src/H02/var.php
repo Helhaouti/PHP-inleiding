11c1644665c051fc790edee107319b3606e00e64
Helhaouti <H.haouti@hotmail.com>
2020-03-25 20:33:49 +0100

    H02

---
+++ Src/H02/var.php
@@ -0,0 +1,18 @@
+<?php
+$a1 = "Ik heb niet gefaald";
+$b1 = "die niet werken";
+$c1 = "10000";
+
+echo $a1 . ". Ik heb alleen " . $c1 . " manieren " . $b1 . "<br>";
+
+$a2 = "die geen fout";
+$b2 = "en";
+$c2 = "niet.";
+
+echo "Mensen " . $a2 . $b2 . " maken, werken " . $c2 . "<br>";
+
+$a3 = "ry";
+$b3 = ", but";
+$c3 = "fail";
+
+echo "T" . $a3 . " and " . $c3 . $b3 . " never " . $c3 . " to t" . $a3 . ".";
